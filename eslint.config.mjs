e5b6a447522d81be681c7f960e5d617a85f53e64
Rodel <rodel.crisosto7@gmail.com>
2025-04-17 21:04:42 +0800

    chore: add button ui & storybook & initially setup color scheme

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,43 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    vars: "all",
+                    args: "all",
+                    ignoreRestSiblings: true,
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    caughtErrorsIgnorePattern: "^_"
+                }
+            ],
+            "@typescript-eslint/no-empty-interface": "warn",
+            "@typescript-eslint/no-empty-object-type": "warn",
+            "import/order": [
+                "warn",
+                {
+                    groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+                    alphabetize: { order: "asc", caseInsensitive: true }
+                }
+            ],
+            "@typescript-eslint/consistent-type-imports": [
+                "warn",
+                {
+                    prefer: "type-imports",
+                    disallowTypeAnnotations: false
+                }
+            ]
+        }
+    }
 ];
 
 export default eslintConfig;
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,54 @@
+import { cva, type VariantProps } from "cva";
+import type { ComponentProps } from "react";
+
+const button = cva("cursor-pointer", {
+    variants: {
+        variant: {
+            primary: `text-background bg-primary hover:bg-primary-400 focus:ring-4 focus:ring-blue-300
+                      focus:outline-none`,
+            fade: "text-primary bg-primary/10 hover:bg-primary/20",
+            outline: " text-primary border-2 border-primary hover:bg-primary/10",
+            destructive:
+                "bg-destructive text-background hover:bg-destructive-600 focus:ring-4 focus:ring-destructive/20",
+            "destructive-fade": "text-destructive bg-destructive/10 hover:bg-destructive/20",
+            ghost: "text-primary bg-transparent hover:bg-primary/10"
+        },
+        size: {
+            sm: "text-sm h-9 text leading-0",
+            base: "text-base py-2 px-4",
+            md: "text-base py-3 px-6",
+            lg: "text-md py-4 px-8"
+        },
+        radius: {
+            md: "rounded-md",
+            lg: "rounded-lg",
+            xl: "rounded-xl",
+            full: "rounded-full"
+        }
+    },
+    defaultVariants: {
+        variant: "primary",
+        size: "base",
+        radius: "lg"
+    }
+});
+
+type BaseButtonProps = ComponentProps<"button">;
+
+interface ButtonProps extends VariantProps<typeof button>, BaseButtonProps {
+    title?: string;
+}
+
+const Button = ({ variant, size, radius, title, className, ...props }: ButtonProps) => {
+    return (
+        <button
+            type="button"
+            className={`${button({ variant, size, radius })} ${className}`}
+            {...props}
+        >
+            {title}
+        </button>
+    );
+};
+
+export default Button;
